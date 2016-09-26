7ae5f767d417d893c5a1eb64e497df5d81e17177
yunsik <wkdrn008@gmail.com>
2016-09-26 14:21:46 +0900

    sdfasfd
    asfasdf

--- src/main.java
+++ src/main.java
@@ -3,9 +3,9 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("변지훈 테스트 완료");
+		System.out.println("qwerqwerqwerqwer");
 		// TODO Auto-generated method stub
-		System.out.println("노희원 이용한 박태양 노답데쓰");
+		System.out.println("노희원 이용한 박태양 노답데쓰");		
 	}
 
 }
