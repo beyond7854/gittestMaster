17f6f8198c65ab0286c1a188a66073327fa417b1
toolbee <wlgns309@gmail.com>
2016-09-26 14:27:54 +0900

    변지훈-장윤식 충돌
    변지훈-장윤식 충돌

--- src/main.java
+++ src/main.java
@@ -6,6 +6,7 @@ public class main {
 		System.out.println("변지훈 테스트 완료");
 		// TODO Auto-generated method stub
 		System.out.println("노희원 이용한 박태양 노답데쓰를 변지훈이 충돌내기");
+		System.out.println("변지훈이 윤식이형과 충돌내기");
 	}
 
 }
