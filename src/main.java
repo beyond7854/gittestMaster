5752f43e985d6d2398f163beebf3b5f95e75ffe9
qwer522 <wldnjssla111@naver.com>
2016-09-26 14:27:02 +0900

    ㅁㄴㅇㄹ
    ㄴㅇㅁㄹ

--- src/main.java
+++ src/main.java
@@ -5,10 +5,9 @@ public class main {
 		
 		System.out.println("변지훈 테스트 완료");
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
+
 		System.out.println("노희원 이용한 박태양 노답데쓰를 변지훈이 충돌내기");
-=======
->>>>>>> parent of e5cc314... 이거왜안떠
+
 	}
 
 }
